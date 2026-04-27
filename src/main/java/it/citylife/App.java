3182f7c3fddd5aca6b7ea4297b74d04ca88c42f4
Simone <simone@Laptop.Simone>
2026-04-27 09:13:25 +0200

    Primo commit: progetto creato

---
+++ src/main/java/it/citylife/App.java
@@ -0,0 +1,13 @@
+package it.citylife;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
